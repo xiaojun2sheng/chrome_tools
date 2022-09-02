30e50166150d033a57337f2c589c22e255487dc0
王军杰 <865677216@qq.com>
2022-09-02 10:19:00 +0800

    init

--- background.js
+++ background.js
@@ -1,16 +1,4 @@
-/**
- * request请求，不可以引入jQuery，使用原生xmlHttpRequest
- */
-function _request (url, data) {
-	const xhr = new XMLHttpRequest()
-	xhr.open('POST',url,true)
-	xhr.setRequestHeader('content-type','application/json')
-	xhr.responseType = 'json'
-	xhr.send(JSON.stringify(data))
-}
-
 // chrome - 通讯
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
-	this._request(`${request.host}/accept/dealData`, request.params)
 });
--- js/host.js
+++
@@ -1,2 +0,0 @@
-const _httpsHost = 'https://192.168.30.24:8889' 
-const _httpHost = 'http://192.168.30.24:8890'
